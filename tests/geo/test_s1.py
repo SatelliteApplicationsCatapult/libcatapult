1529964fb4c53d2112924e807c47f21004eb5bdf
Emily <emily.selwood@sa.catapult.org.uk>
2020-01-21 16:18:43 +0000

    more tests

--- tests/geo/test_s1.py
+++ tests/geo/test_s1.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime
 
-from libcatapult.geo.s1 import S1Product
+from libcatapult.geo.s1 import S1Product, validate, common_polarisations
 
 
 class TestS1Product(unittest.TestCase):
@@ -14,3 +14,25 @@ class TestS1Product(unittest.TestCase):
         self.assertEqual(result.start_timestamp(), datetime.strptime("20191130T173211", "%Y%m%dT%H%M%S"), "incorrect start time")
         self.assertEqual(result.stop_timestamp(), datetime.strptime("20191130T173240", "%Y%m%dT%H%M%S"), "incorrect stop time")
 
+
+class TestS1Validate(unittest.TestCase):
+
+    def test_invalid_names(self):
+        self.assertFalse(validate(""))
+        self.assertFalse(validate("kjsfdagufhawlseqnxu32a\\wrhyuirygb no"))
+        self.assertFalse(validate("            "))
+
+    def test_valid_name(self):
+        self.assertTrue(validate("S1A_IW_GRDH_1SDV_20191130T173211_20191130T173240_030143_0371B4_E057"))
+
+
+class TestS1CommonPolarisations(unittest.TestCase):
+
+    def test(self):
+        input = [
+            S1Product("S1A_IW_GRDH_1SDV_20191130T173211_20191130T173240_030143_0371B4_E057"),
+            S1Product("S1A_IW_GRDH_1SSV_20191130T173211_20191130T173240_030143_0371B4_E057"),
+        ]
+
+        result = common_polarisations(input)
+        self.assertListEqual(result, ['vv'])
